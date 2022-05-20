1192382a1032ec427025e0b75f45e5b056b008bd
Aznavyr <cesar-88@mail.ru>
2022-05-20 13:25:15 +0300

    FailedTest

---
+++ src/test/java/ru/netology/test/TestApi.java
@@ -0,0 +1,29 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class TestApi {
+
+    @Test
+    public void shouldCheckBodyResponce() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("10.06.1988") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                        .
+
+                body("data", equalTo("someData"));
+
+
+    }
+}
